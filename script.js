6b13ed82ff9eb74a8f1c1dd0a08a2a396675c97a
Sady Guzman <102682450+Sady-Guzman@users.noreply.github.com>
2025-11-17 14:14:07 -0300

    Update survey link to new Google Forms URL

--- script.js
+++ script.js
@@ -217,7 +217,7 @@ document.addEventListener("DOMContentLoaded", () => {
         <a href="assets/SadyGuzman_CV_2025_en.pdf" target="_blank" class="cv-button">📄 Download CV (PDF)</a>
         <br><br><br>
         <h1>ENCUESTA DII2<h1>
-        <a href="https://forms.gle/it7efeeYfvGE26JN8">LINK A ENCUESTA</a>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdzB5kyLWAC0lLft9_bsVgWsX-YCFXPv4lNJkOjIVZWVYCm0A/viewform?usp=dialog">LINK A ENCUESTA</a>
       `,
       formacion: `
         <h2>Education</h2>
